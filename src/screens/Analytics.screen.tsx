c4139ba7684e435b6ef8b999a5b93f5386b9058e
Mritunjay ZL <mritunjay.saha@ziroh.com>
2023-07-04 15:52:35 +0530

    feat: added analytics screen

---
+++ src/screens/Analytics.screen.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+export const Analytics = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Analytics</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
--- src/screens/BottomTabs.navigator.tsx
+++ src/screens/BottomTabs.navigator.tsx
@@ -1,6 +1,7 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Home } from './Home.screen';
 import { History } from './History.screen';
+import { Analytics } from './Analytics.screen';
 
 const BottomTabs = createBottomTabNavigator();
 
@@ -9,6 +10,7 @@ export const BottomTabsNavigator = () => {
     <BottomTabs.Navigator>
       <BottomTabs.Screen name="Home" component={Home} />
       <BottomTabs.Screen name="History" component={History} />
+      <BottomTabs.Screen name="Analytics" component={Analytics} />
     </BottomTabs.Navigator>
   );
 };
