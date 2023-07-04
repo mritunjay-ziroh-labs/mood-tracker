32d4ec0839f23ac10c40abdf984d06e2dbf21d81
Mritunjay ZL <mritunjay.saha@ziroh.com>
2023-07-04 15:47:33 +0530

    feat: implemented two tabs layout
    added home and history tabs

---
+++ src/screens/BottomTabs.navigator.tsx
@@ -0,0 +1,14 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Home } from './Home.screen';
+import { History } from './History.screen';
+
+const BottomTabs = createBottomTabNavigator();
+
+export const BottomTabsNavigator = () => {
+  return (
+    <BottomTabs.Navigator>
+      <BottomTabs.Screen name="Home" component={Home} />
+      <BottomTabs.Screen name="History" component={History} />
+    </BottomTabs.Navigator>
+  );
+};
